5785ec56aecbeeb384c5a9b36cc3c895b3069edb
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-10 16:26:43 -0700

    Added API call for SearchResult

--- src/services/moviesAPI.js
+++ src/services/moviesAPI.js
@@ -1,8 +1,9 @@
 const BASE_URL = 'https://api.themoviedb.org/3/movie/now_playing?';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?';
 const apiKey = import.meta.env.VITE_APP_API_KEY;
 
-export const fetchNowPlaying = async(page=1, title = "")  => {
-    const url = `${BASE_URL}api_key=${apiKey}&page=${page}&title=${title}`
+export const fetchNowPlaying = async(page=1)  => {
+    const url = `${BASE_URL}api_key=${apiKey}&page=${page}`
     try {
         const response = await fetch(url);
         if(!response.ok){
@@ -16,3 +17,18 @@ export const fetchNowPlaying = async(page=1, title = "")  => {
     }
     
 };
+
+export const searchResult = async(query) => {
+    const url = `${SEARCH_URL}api_key=${apiKey}&query=${query}&include_adult=false&language=en-US&page=1`
+    try {
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error('Failed to fetch movies')
+        }
+        const searchData = await response.json();
+        return searchData.results;
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        throw error;
+    }
+}
