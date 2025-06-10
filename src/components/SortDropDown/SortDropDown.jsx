888fb233bb81eb62334f2a17d3f405b1e743097d
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-10 16:25:07 -0700

    Changed onSortChange to onChange

--- src/components/SortDropDown/SortDropDown.jsx
+++ src/components/SortDropDown/SortDropDown.jsx
@@ -1,5 +1,4 @@
 
-
 const sortOptions = [
     {label: "Title (A-Z)", value:"title"},
     {label: "Release date (Newest-Oldest)", value:"release_date"},
@@ -12,7 +11,7 @@ export default function SortDropDown( {onSortChange} ){
         onSortChange(e.target.value);
     }
     return (
-        <select onSortChange={handleSort}>
+        <select onChange={handleSort}>
             <option value="">Sort by</option>
             {sortOptions.map((option) => (
                 <option key={option.value} value={option.value}>{option.label}</option>
