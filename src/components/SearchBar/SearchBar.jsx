a5520d79705bb12f8f710157b7f3e81eb709a00c
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-10 16:23:23 -0700

    Moved SearchBar props to Home

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,41 @@
 import React,{useState} from "react"
-import {FaSearch} from 'react-icons/fa'
+import {FaSearch, FaTimes} from 'react-icons/fa'
 import './SearchBar.css'
 
 
-export default function SearchBar({onSearch}){
+export default function SearchBar({onSearch, onSearchClick}){
     const [searchQuery, setSearchQuery] =  useState("")
+    const [showClearButton, setShowClearButton] = useState(false)
 
     const handleSearchChange = (event) => {
         setSearchQuery(event.target.value);
+        setShowClearButton(event.target.value.length > 0)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(searchQuery.trim()){
+        if(searchQuery){
+            setSearchQuery(searchQuery.trim())
             onSearch(searchQuery.trim());
         }
     };
+
+    // const handleKeyPress = (e) => {
+    //     if(e.key === 'Enter' && searchQuery){
+    //         setSearchQuery(searchQuery.trim())
+    //         onSearch(searchQuery.trim());
+    //     }
+    // };
+
+    const handleClick = () => {
+        onSearchClick();
+    }
+
+    const handleClear = (e) => {
+        setSearchQuery('');
+        onSearch('');
+        setShowClearButton(false)
+    }
 
     return (
         <form onSubmit={handleSubmit} className="search-container">
@@ -23,11 +43,17 @@ export default function SearchBar({onSearch}){
                 type="text"
                 placeholder="Search for movies..."
                 value={searchQuery}
+                onClick={handleClick}
+                // onKeyDown={handleKeyPress}
                 onChange={handleSearchChange}
             />
-            <button type="submit">
-                <FaSearch />
-            </button>
+            {showClearButton && 
+            <button onClick={handleClear}><FaTimes /></button>}
+
+            <button type="submit"><FaSearch /></button>
+
+
+            
         </form>
     )
 }
