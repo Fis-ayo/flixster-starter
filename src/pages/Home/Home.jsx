0005a962f7682534b968e918f87f44604fea7ca8
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-11 10:41:14 -0700

    Merged SearchBar updated feature

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,71 +1,82 @@
-import { fetchNowPlaying, searchResult } from "../../services/moviesAPI"
-import Header from "../../components/Header/Header"
-import Footer from "../../components/Footer/Footer"
-import MovieList from "../../components/MovieList/MovieList"
-import SearchBar from "../../components/SearchBar/SearchBar"
-import SortDropDown from "../../components/SortDropDown/SortDropDown"
-import NowPlaying from "../../components/SearchBar/NowPlaying"
-import React, { useState, useEffect} from "react"
-
+import { fetchNowPlaying, searchResult } from "../../services/moviesAPI";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import MovieList from "../../components/MovieList/MovieList";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import SortDropDown from "../../components/SortDropDown/SortDropDown";
+import NowPlaying from "../../components/SearchBar/NowPlaying";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
-    const [movies, setMovies] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [page, setPage] = useState(1)
-    const [playingActive, setPlayingActive] = useState(true);
-    const [searchQuery, setSearchQuery] = useState("")
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [playingActive, setPlayingActive] = useState(true);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const getData = async () =>{
-            const results = await fetchNowPlaying(page);
-            setMovies(results);
-            setLoading(false);
+  const getData = async () => {
+    const results = await fetchNowPlaying(page);
+    setMovies(results);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleSearch = async (query) => {
+    if (query === "") {
+      setPlayingActive(false);
+    } else {
+      const results = await searchResult(query);
+      setPlayingActive(true);
+      if (results.length === 0) {
+        console.log("No result found"); //Fix
+      } else {
+        setMovies(results);
+      }
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        getData();
-    }, []);
+  const handleSearchClick = () => {
+    setPlayingActive(false);
+  };
 
-    const handleSearch = async(query) => {
-        if(query === ''){
-            setPlayingActive(false)
-        }else{
-            const results = await searchResult(query);
-            setPlayingActive(true)
-            if(results.length === 0){
-                console.log ("No result found") //Fix
-            }else{setMovies(results);}
-            setLoading(false);
-        }  
+  const handleNowPlayingClick = () => {
+    getData();
+    if (searchQuery) {
+      setSearchQuery("");
     }
+    setPlayingActive(true);
+  };
 
-    const handleSearchClick = () => {
-        setPlayingActive(false);
-    }
+  const handleSortChange = (value) => {};
 
-    const handleNowPlayingClick = () => {
-        getData();
-        if(searchQuery){
-            setSearchQuery('')
-        }
-        setPlayingActive(true)
-    }
+  return (
+    <div>
+      <Header />
+      <NowPlaying onNowPlayingClick={handleNowPlayingClick} />
+      <SearchBar
+        onSearch={handleSearch}
+        onSearchClick={handleSearchClick}
+        setSearchQuery={setSearchQuery}
+        searchQuery={searchQuery}
+      />
+      <SortDropDown onSortChange={handleSortChange} />
+      {playingActive ? (
+        <MovieList
+          movies={movies}
+          page={page}
+          setPage={setPage}
+          setMovies={setMovies}
+          loading={loading}
+        />
+      ) : (
+        <p>Search for Movies</p>
+      )}
 
-    const handleSortChange = (value) => {
-
-    }
-
-    return (
-        <div>
-            <Header />
-            <NowPlaying onNowPlayingClick={handleNowPlayingClick}/>
-            <SearchBar onSearch={handleSearch} onSearchClick={handleSearchClick} setSearchQuery={setSearchQuery} searchQuery={searchQuery}/>
-            <SortDropDown onSortChange={handleSortChange}/>
-            {playingActive ?
-            <MovieList movies={movies} page={page} setPage = {setPage} setMovies={setMovies} loading={loading}/>:
-            <p>Search for Movies</p>
-            }
-            
-            <Footer />
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 }
--- src/services/moviesAPI.js
+++ src/services/moviesAPI.js
@@ -1,34 +1,33 @@
-const BASE_URL = 'https://api.themoviedb.org/3/movie/now_playing?';
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?';
+const BASE_URL = "https://api.themoviedb.org/3/movie/now_playing?";
+const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?";
 const apiKey = import.meta.env.VITE_APP_API_KEY;
 
-export const fetchNowPlaying = async(page=1)  => {
-    const url = `${BASE_URL}api_key=${apiKey}&page=${page}`
-    try {
-        const response = await fetch(url);
-        if(!response.ok){
-            throw new Error('Failed to fetch movies')
-        }
-        const data = await response.json();
-        return data.results;
-    } catch (error) {
-        console.error('Error fetching movies:', error);
-        throw error;
+export const fetchNowPlaying = async (page = 1) => {
+  const url = `${BASE_URL}api_key=${apiKey}&page=${page}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch movies");
     }
-    
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    throw error;
+  }
 };
 
-export const searchResult = async(query) => {
-    const url = `${SEARCH_URL}api_key=${apiKey}&query=${query}&include_adult=false&language=en-US&page=1`
-    try {
-        const response = await fetch(url);
-        if(!response.ok){
-            throw new Error('Failed to fetch movies')
-        }
-        const searchData = await response.json();
-        return searchData.results;
-    } catch (error) {
-        console.error('Error fetching movies:', error);
-        throw error;
+export const searchResult = async (query) => {
+  const url = `${SEARCH_URL}api_key=${apiKey}&query=${query}&include_adult=false&language=en-US&page=1`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch movies");
     }
-}
+    const searchData = await response.json();
+    return searchData.results;
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    throw error;
+  }
+};
