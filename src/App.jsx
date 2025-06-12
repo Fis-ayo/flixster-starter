6e1434e80e3737b207540fcfb13d69548525cf6b
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-12 15:25:03 -0700

    Routed the pages to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Favorites from "./pages/Favorites/Favorites";
+import Watched from "./pages/Watched/Watched";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
+
 const App = () => {
+
+
   return (
     <div className="App">
-      <Home />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="src/pages/Favorites/Favorites.jsx" element={<Favorites />} />
+        <Route path="src/pages/Watched/Watched.jsx" element={<Watched />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
-import "./Header.css"
+import "./Header.css";
 import SideBar from "../SideBar/SideBar";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 export default function Header() {
+  const [clickedMenu, setClickedMenu] = useState(false);
+
+  const onMenuClick = () => {
+    setClickedMenu(true);
+  }
+
   return (
-    <div className="header-container">
-      <h1>🎞️Flixster</h1>
-      <SideBar />
-    </div>
+    <>
+      <div className="header-container">
+        <h1>🎞️Flixster</h1>
+        <FaBars className="menu" onClick={onMenuClick} />
+      </div>
+      {clickedMenu && <SideBar onMenuClose={() => setClickedMenu(false)}/>}
+    </>
   );
 }
