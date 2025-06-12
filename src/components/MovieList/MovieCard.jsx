8e4647cde421a68cf228deff7af889254457ae73
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-11 19:47:43 -0700

    Fixed bugs and added style  to header and footer

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -31,4 +31,3 @@ export default function MovieCard({item, onClick}) {
         </>
     );
 }
-
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,7 +2,14 @@ import { FaBars } from "react-icons/fa";
 
 export default function SideBar(){
     return (
+        <>
         <FaBars />
+        <li>
+        <a><Home /></a>
+        <a><Favorites /></a>
+        <a><Watched /></a>
+        </li>
+        </>
     )
 
 }
