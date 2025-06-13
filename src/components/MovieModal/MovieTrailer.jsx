405bb5d701f31bb8f9cbf3070b0b471d843a436c
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-13 16:55:24 -0700

    Fixed page responsiveness

--- src/components/MovieModal/MovieTrailer.jsx
+++ src/components/MovieModal/MovieTrailer.jsx
@@ -15,7 +15,7 @@ export default function MovieTrailer({ movie }) {
         <iframe
           width="560"
           height="315"
-          src={`https://www.youtube.com/embed/${movie.trailerKey}?autoplay=1`}
+          src={`https://www.youtube.com/embed/${movie.trailerKey}`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
