2778961b2fef393601982b9b701d906b603e611e
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-13 11:40:43 -0700

    Refactored Content action to a more suitable folder

---
+++ src/components/ToolBar/SearchBar.jsx
@@ -0,0 +1,63 @@
+import { FaSearch, FaTimes } from "react-icons/fa";
+import "./SearchBar.css";
+
+export default function SearchBar({
+  onSearch,
+  onSearchClick,
+  searchQuery,
+  setSearchQuery,
+}) {
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchQuery) {
+      setSearchQuery(searchQuery.trim());
+      onSearch(searchQuery.trim());
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (searchQuery) {
+        setSearchQuery(searchQuery.trim());
+        onSearch(searchQuery.trim());
+      }
+    }
+  };
+
+  const handleClick = () => {
+    onSearchClick();
+  };
+
+  const handleClear = (e) => {
+    setSearchQuery("");
+    onSearch("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="search-container">
+      <button className="search-btn" type="submit">
+        <FaSearch className="search-icon"/>
+      </button>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search for movies..."
+        value={searchQuery}
+        onClick={handleClick}
+        onKeyDown={handleKeyPress}
+        onChange={handleSearchChange}
+      />
+        <button className="clear-btn" onClick={handleClear}>
+          <FaTimes />
+        </button>
+
+      
+    </form>
+  );
+}
